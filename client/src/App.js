ea4a297ca85105e058938bb2a6a9bfa84eb86452
Ksis123 <khemkhemsiwa555@gmail.com>
2023-12-08 00:16:08 +0700

    Update init

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <a target='_blank'>
+          {/* <img width="1680" alt="image" src="" ></img> */}
+        </a>
         <h1> Fair Manpower</h1>
       </header>
     </div>
